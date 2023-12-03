e6b6b1a8383489ba0bba7d0aca3cc4cb48069881
jeanniechen <jcgriendt@gmail.com>
2023-12-03 11:12:54 -0800

    Display with Vue templating for conditional and list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <script setup>
-// reactive for objects, not JS primitives
-// used for the state when there's a group of related data
-// ref uses reactive behind the scenes
-import { reactive } from 'vue'
+import { ref } from 'vue'
+import socksGreenImage from './assets/images/socks_green.jpeg'
 
-const product = reactive({
-  name: 'Socks'
-})
+const product = ref('Socks')
+const image = ref(socksGreenImage)
+const popularItem = ref(true)
+const inventory = ref(8)
 
-// reassignment
-product.name = "New Socks"
+const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const variants = ref([
+  { id: 2234, color: 'green' },
+  { id: 2235, color: 'blue' }
+])
 </script>
 
 <template>
-  <h1>{{ product.name }}</h1>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+
+        <!-- Conditional Rendering -->
+        <!-- v-show is similar to v-if but renders and hides using display:none, so more performant if the code needs to appear and hide frequently-->
+        <p v-show="popularItem">Popular Item!</p>
+
+        <!-- v-if and v-else needs to be used on elements next to each other -->
+        <p v-if="inventory > 10">In Stock</p>
+        <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+        <p v-else>Out of Stock</p>
+
+        <ul>
+          <!-- details must be iterable to be compatible with v-for -->
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <!-- v-for and v-if can appear on the same element, however v-if is always processed first regardless of order-->
+        <!-- Vue also relies on the virtual DOM similar to React so the key is required -->
+        <div v-for="variant in variants" v-bind:key="variant.id">{{ variant.color }}</div>
+      </div>
+    </div>
+  </div>
 </template>
