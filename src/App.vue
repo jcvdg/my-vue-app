0030c2b8abc97de7509b31386776b8287f193534
jeanniechen <jcgriendt@gmail.com>
2023-12-03 00:40:51 -0800

    Use reactive

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <script setup>
-// ref is same as useState
-// ref is for reference to reactivity, not ref to object, so our state is the ref object.
-// for JS primitive types
+// reactive for objects, not JS primitives
+// used for the state when there's a group of related data
+// ref uses reactive behind the scenes
+import { reactive } from 'vue'
 
-// The idea is that the value 'Socks', a string, is not a reference
-// However, we need a reference object to facilitate reactivity in a Vue component.
-// e.g. When we change a variable value from 'Socks' to 'New Socks', we're reassigning the variable to a different value.
-//      Then the original value of 'Socks' is lost, and thus the reactivity is lost as well.
-//      => We wrap the 'Socks' value in an object with ref.
-// Our state is the ref object "ref('Socks')", and not the string value.
-import { ref } from 'vue'
-
-const product = ref('Socks')
+const product = reactive({
+  name: 'Socks'
+})
 
 // reassignment
-product.value = 'New Socks'
+product.name = "New Socks"
 </script>
 
 <template>
-  <h1>{{ product }}</h1>
+  <h1>{{ product.name }}</h1>
 </template>
